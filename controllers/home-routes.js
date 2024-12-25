ef82dda3cfc776a8d5c512338aee0d9ebc767c68
Tony Magrady <tonymagrady@gmail.com>
2024-12-24 18:28:53 -0800

    update package*.json files

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,20 +3,31 @@ const {Blogpost, Comment} = require('../models');
 
 // GET all blog posts for homepage
 router.get('/', async (req, res) => {
+  // try {
+  //   const dbBlogData = await Blogpost.findAll();
+
+  //   const blogPosts = dbBlogData.map((blogPost) =>
+  //     blogPost.get({plain: true})
+  //   );
+  //   // Send over the 'loggedIn' session variable to the 'homepage' template
+  //   res.render('homepage', {
+  //     blogPosts,
+  //     loggedIn: req.session.loggedIn,
+  //   });
+  // } catch (err) {
+  //   console.log(err);
+  //   res.status(500).json(err);
+  // }
   try {
     const dbBlogData = await Blogpost.findAll();
-
-    const blogPosts = dbBlogData.map((blogPost) =>
-      blogPost.get({plain: true})
-    );
-    // Send over the 'loggedIn' session variable to the 'homepage' template
+    const blogPosts = dbBlogData.map((blogPost) => blogPost.get({ plain: true }));
     res.render('homepage', {
       blogPosts,
       loggedIn: req.session.loggedIn,
     });
   } catch (err) {
-    console.log(err);
-    res.status(500).json(err);
+    console.error('Error fetching blog posts:', err);
+    res.status(500).json({ error: 'Internal Server Error' });
   }
 });
 
